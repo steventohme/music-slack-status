c9f45a0871eaebb531682576bcfe836cd842a32f
Steven Tohme <steventohme59@gmail.com>
2024-10-24 23:14:03 -0400

    update set_status

--- slack/slack.py
+++ slack/slack.py
@@ -1,8 +1,10 @@
 from slack_sdk import WebClient
 
-def set_status(client: WebClient, status_text: str, status_emoji: str):
+
+def set_status(client: WebClient, user_id:str,  status_text: str, status_emoji: str, status_expiration: int):
     try:
         client.users_profile_set(
+            user=user_id,
             profile={
                 "status_text": status_text,
                 "status_emoji": status_emoji,
