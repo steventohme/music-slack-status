c45f3feb6755c3725b86b53ee0e4bedad84ab25f
Steven Tohme <steventohme59@gmail.com>
2024-10-25 00:16:43 -0400

    create main script

---
+++ main.py
@@ -0,0 +1,22 @@
+from slack.slack import SlackClient
+from spotify.spotify import SpotifyClient
+import sys
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <user-id>")
+        sys.exit(1)
+
+
+    spotify = SpotifyClient()
+
+    status_text = spotify.get_song_status()
+    status_emoji = ":notes:"
+    status_expiration = 0
+
+    user_id = sys.argv[1]
+    slack = SlackClient()
+    slack.set_status(user_id, status_text, status_emoji, status_expiration)
+
+if __name__ == "__main__":
+    main()
