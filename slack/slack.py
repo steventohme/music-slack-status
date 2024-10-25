f5116baf3e77462321ee8fab694e72f7092b03fb
Steven Tohme <steventohme59@gmail.com>
2024-10-25 00:16:15 -0400

    create SlackClient class

--- slack/slack.py
+++ slack/slack.py
@@ -1,37 +1,25 @@
 from slack_sdk import WebClient
 from dotenv import load_dotenv
-import sys
 import os
 
 load_dotenv()
 
-def set_status(client: WebClient, user_id:str,  status_text: str, status_emoji: str, status_expiration: int):
-    try:
-        client.users_profile_set(
-            user=user_id,
-            profile={
-                "status_text": status_text,
-                "status_emoji": status_emoji,
-            }
-        )
-    except: 
-        print(f"Failed to set status")
+class SlackClient:
+    def __init__(self):
+        token = os.getenv('SLACK_USER_TOKEN')
 
-def main():
-    if len(sys.argv) != 3:
-        print("Usage: python script.py <user-id> '<song-name>'")
-        sys.exit(1)
+        client = WebClient(token=token)
 
-    token = os.getenv('SLACK_USER_TOKEN')
+        self.client = client
 
-    client = WebClient(token=token)
-
-    status_text = f"Listening to: {sys.argv[2]}"
-    status_emoji = ":notes:"
-    status_expiration = 0
-
-    user_id = sys.argv[1]
-    set_status(client, user_id, status_text, status_emoji, status_expiration)
-
-if __name__ == "__main__":
-    main()
+    def set_status(self, user_id:str,  status_text: str, status_emoji: str, status_expiration: int):
+        try:
+            self.client.users_profile_set(
+                user=user_id,
+                profile={
+                    "status_text": status_text,
+                    "status_emoji": status_emoji,
+                }
+            )
+        except: 
+            print(f"Failed to set status")
